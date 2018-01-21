886e6d46e957a3c3bd3d2c3624fd0669ebe6fca5
shuvamoy <shuvamoy1983@gmail.com>
2018-01-21 01:24:18 -0600

    Added Ec2 instance code

--- EC2/EC2_Operation.py
+++ EC2/EC2_Operation.py
@@ -1,25 +1,42 @@
 from Resource.ResouceSelection import resource_name
 
-
-class EC2:
+class StartInstances:
     ec2_conn = resource_name('ec2')
 
+    def __init__(self,keypair):
+        self.keypair=keypair
 
-    def __init__(self,ami_id,key_nm,instance_type):
-        self.ami_id =ami_id
-        self.key_nm= key_nm
-        self.instance_type= instance_type
-        #self.security_id = security_id
 
-    def create_instance_ec2(self):
-       EC2.ec2_conn.run_instances(ImageId= self.ami_id,
-                                     KeyName =self.key_nm,
-                                     InstanceType= self.instance_type,
+    def create_ec2(self):
+        amiNameArr = ["ami-26ebbc5c"]
+        amiDescArr = ["Amazon Linux","Red Hat Enterprise","SUSE Enterprise",
+              "Ubuntu Server 13.10","Microsoft Server 2012"]
+        amiInstTypesArr = ["t1.micro"]
+        print("Creating key pair...")
+        awsKeyPair =StartInstances.ec2_conn.create_key_pair(KeyName=self.keypair)
+        print(awsKeyPair)
+        path = "/Users/shuvamoymondal/"+self.keypair
+        f = open(path, "w")
+        f.write(str(awsKeyPair))
+        print("Saved key pair: " + self.keypair)
+        f.close()
 
-                                     MinCount=1,
-                                     MaxCount=1)
+        print("Spawning instances...")
+
+        for amiIndx in range(1):  # len(amiNameArr)):
+         print("   AMI description: " + str(amiDescArr[amiIndx]))
+         for typeIndx in range(1):  # len(amiInstTypesArr)):
+           print("      starting machine: " + str(amiInstTypesArr[typeIndx]))
+           StartInstances.ec2_conn.run_instances(
+            ImageId=amiNameArr[amiIndx],
+            InstanceType=amiInstTypesArr[typeIndx],
+           # SecurityGroups='default',
+            KeyName=self.keypair,
+            MinCount=1,
+            MaxCount=1
+        )
 
 
 if __name__ == "__main__":
-    p = EC2('ami-0b1e356e','myec2','t2.micro')
-    print(p.create_instance_ec2())
+    v = StartInstances("mykeypair.pem")
+    v.create_ec2()
