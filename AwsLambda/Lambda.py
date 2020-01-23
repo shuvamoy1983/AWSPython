de2850f43dabf7f5e8d1366a20a6413ea4ff86a4
Mondal <shumondal@deloitte.com>
2020-01-24 00:38:42 +0530

    Added code for Aws Glue

--- AwsLambda/Lambda.py
+++ AwsLambda/Lambda.py
@@ -3,6 +3,6 @@ client = boto3.client('lambda')
 response = client.create_function(
     FunctionName='LambdaToGlueExecution',
     Runtime='python3.6',
-    Role='arn:aws:iam::655700933854:role/LambdaFullAccess',
+    Role='arn:aws:iam::655700:role/LambdaFullAccess',
     Handler='LambdaToGlueExecution.lambda_handler',
     Code= {'ZipFile': open(r'C:\Users\shumondal\PycharmProjects\Lambda\mylambda.zip', 'rb').read() })
