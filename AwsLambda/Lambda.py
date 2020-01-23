3935b939559b539d5c879b93360f328cfe0f1af3
Mondal <shumondal@deloitte.com>
2020-01-23 20:37:45 +0530

    Added Lambda Code using boto

--- AwsLambda/Lambda.py
+++ AwsLambda/Lambda.py
@@ -3,6 +3,6 @@ client = boto3.client('lambda')
 response = client.create_function(
     FunctionName='mylambda',
     Runtime='python3.6',
-    Role='arn:aws:iam::655700933854:role/LambdaFullAccess',
+    Role='arn:aws:iam::655:role/LambdaFullAccess',
     Handler='mylambda.lambda_handler',
     Code= {'ZipFile': open(r'C:\Users\shumondal\PycharmProjects\Lambda\mylambda.zip', 'rb').read() })
